b65856e0de967fb7e04faeefe8f00b1c91963b14
Roni Dutta <ronisanker@gmail.com>
2016-03-28 11:44:18 +0600

    ajax and responsive all is done

--- wordpress/wp-content/themes/twentysixteen/js/roni.js
+++ wordpress/wp-content/themes/twentysixteen/js/roni.js
@@ -1,7 +1,178 @@
+/* <a href="http://roni.localhost/" style="
+    right: 0px;
+    position: absolute;
+    top: 0;
+"><img class="wp-image-82 size-full alignright" src="http://roni.localhost/wp-content/uploads/2016/03/close.png" alt="back" width="59" height="58" style="
+    right: 0px;
+"></a> */
+
+
+/* page 1 */
 $(document).ready(function(){
     $("#news1").click(function(){
-        $.ajax({url: "http://localhost/caohagan_r/wordpress/?page_id=38 ", success: function(result){
-            $("#wrapper-outer").html(result);
+        $.ajax({url: "http://roni.localhost/?page_id=38 ", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 2 */
+$(document).ready(function(){
+    $("#news2").click(function(){
+        $.ajax({url: " http://roni.localhost/?page_id=41", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 3 */
+$(document).ready(function(){
+    $("#idx-cont_fig01 > article > div > div.figTxt > div.nav > ul > li:nth-child(1) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=43", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 4 */
+$(document).ready(function(){
+    $("#idx-cont_fig01 > article > div > div.figTxt > div.nav > ul > li:nth-child(2) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=45", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 5 */
+$(document).ready(function(){
+    $("#idx-cont_fig01 > article > div > div.figTxt > div.nav > ul > li:nth-child(3) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=47", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 6 */
+$(document).ready(function(){
+    $("#idx-cont_fig01 > article > div > div.figTxt > div.nav > ul > li:nth-child(4) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=49", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 7 */
+$(document).ready(function(){
+    $("#idx-cont_fig01 > article > div > div.figTxt > div.nav > ul > li:nth-child(5) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=51", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 8 */
+$(document).ready(function(){
+    $("#idx-cont_fig01 > article > div > div.figTxt > div.nav > ul > li:nth-child(6) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=53", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 9 */
+$(document).ready(function(){
+    $("#idx-cont_fig02 > article > div > div.nav > ul > li:nth-child(1) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=55", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 10 */
+$(document).ready(function(){
+    $("#idx-cont_fig02 > article > div > div.nav > ul > li:nth-child(2) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=57", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 11 */
+$(document).ready(function(){
+    $("#idx-cont_fig02 > article > div > div.nav > ul > li:nth-child(3) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=117", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 12 */
+$(document).ready(function(){
+    $("#idx-cont_fig03 > article > div > div.nav > ul > li:nth-child(1) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=59", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 13 */
+$(document).ready(function(){
+    $("#idx-cont_fig03 > article > div > div.nav > ul > li:nth-child(2) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=59", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 14 */
+$(document).ready(function(){
+    $("#idx-cont_fig03 > article > div > div.nav > ul > li:nth-child(3) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=61", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 15 */
+$(document).ready(function(){
+    $("#idx-cont_fig03 > article > div > div.nav > ul > li:nth-child(4) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=63", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 16 */
+$(document).ready(function(){
+    $("#idx-cont_fig03 > article > div > div.nav > ul > li:nth-child(5) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=67", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 17 */
+$(document).ready(function(){
+    $("#idx-cont_fig03 > article > div > div.nav > ul > li:nth-child(6) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=69", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/*fig 4*/
+/* page 18 */
+$(document).ready(function(){
+    $("#idx-cont_fig04 > article > div > div.nav > ul > li:nth-child(1) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=71", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 19 */
+$(document).ready(function(){
+    $("#idx-cont_fig04 > article > div > div.nav > ul > li:nth-child(2) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=73", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 20 */
+$(document).ready(function(){
+    $("#idx-cont_fig04 > article > div > div.nav > ul > li:nth-child(3) > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=75", success: function(result){
+            $("html").html(result);
+        }});
+    });
+});
+/* page 21 */
+$(document).ready(function(){
+    $("#idx-cont_fig05 > article > div > div.nav > ul > li > a").click(function(){
+        $.ajax({url: "http://roni.localhost/?page_id=38", success: function(result){
+            $("html").html(result);
         }});
     });
 });
